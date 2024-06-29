13ea4e2128a12dd2592e26e2b9e8a15fe194b521
Shashwat Sharma <shashwat.ashutosh.sharma@gmail.com>
2024-06-29 16:58:13 +0530

    add fonts file that contains export for 2 objects of JetBrains Mono with different weight

---
+++ src/fonts/fonts.js
@@ -0,0 +1,13 @@
+import { JetBrains_Mono } from 'next/font/google'
+ 
+export const jetbrains_mono = JetBrains_Mono({
+  subsets: ['latin'],
+  weight: '700',
+  display: 'swap',
+})
+
+export const jetbrains_mono_light = JetBrains_Mono({
+  subsets: ['latin'],
+  weight: '200',
+  display: 'swap',
+})
